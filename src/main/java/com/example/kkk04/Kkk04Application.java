bc5a06858f51fbe6ffc701c5b02f074994ccd179
KSKamburova18 <KSKamburova18@codingburgas.bg>
2022-11-02 14:44:55 +0200

    Initial commit

---
+++ src/main/java/com/example/kkk04/Kkk04Application.java
@@ -0,0 +1,13 @@
+package com.example.kkk04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kkk04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kkk04Application.class, args);
+    }
+
+}
